8b390483afd78ed2dfc79c28cc1d300ff0d26dc3
Zhiqiang Zang <capapoc@gmail.com>
2021-07-05 14:44:15 -0500

    御风杯2021页面更新。

--- archives/md2html.sh
+++ archives/md2html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pandoc ruomeng2021-rules.md --css="/css/buptra.css" --metadata pagetitle="赛制" -s -o rules-and-regulations.html
+pandoc "$1" --css="/css/buptra.css" --metadata pagetitle="赛制" -s -o rules-and-regulations.html
 mv rules-and-regulations.html ../
